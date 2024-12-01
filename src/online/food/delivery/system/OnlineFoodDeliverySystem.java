07509912d900c4d43534e8410938e2c582e22660
Pushpo20 <puspita23105101511@diu.edu.bd>
2024-12-02 01:48:17 +0600

    Initial files

--- src/online/food/delivery/system/OnlineFoodDeliverySystem.java
+++ src/online/food/delivery/system/OnlineFoodDeliverySystem.java
@@ -6,6 +6,7 @@ public class OnlineFoodDeliverySystem {
 
      public static void main(String[] args) {
         System.out.println("Welcome to food Delivery system");
+         System.out.println("Thank you!");
     }
     
 }
