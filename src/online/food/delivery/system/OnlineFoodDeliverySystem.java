fce68413d5afec568dc0e87c9410c7c6afb00dc2
Pushpo20 <puspita23105101511@diu.edu.bd>
2024-12-05 02:17:13 +0600

    Update OnlineFoodDeliverySystem.java

--- src/online/food/delivery/system/OnlineFoodDeliverySystem.java
+++ src/online/food/delivery/system/OnlineFoodDeliverySystem.java
@@ -4,9 +4,7 @@ package online.food.delivery.system;
 
 public class OnlineFoodDeliverySystem {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         // TODO code application logic here
     }
